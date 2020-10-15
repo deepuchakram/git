2970a3ff53ece763667fce0388266f728ab3f0e1
deepuchakram <sudeepthi516@gmail.com>
2020-10-15 17:39:22 +0530

    adding file five

--- listingfiles.sh
+++ listingfiles.sh
@@ -6,7 +6,7 @@ mkdir /tmp/example3
 echo Folder test is created in /tmp
 cd /tmp/example2/
 <<<<<<< HEAD
-touch files file one file two file three file four 
+touch files file one file two file three file four file five
 =======
 touch files file one file two file three file four
 >>>>>>> 824e7e7cd44a67b3e5a87bafb1c77e9f40d69e7c
